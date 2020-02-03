5fdbfb904e86edaa22d88837b03aff53e04fda86
jravel <jravel@53118b7d-0bdd-47fb-8e37-54b3e301ef77>
2020-02-03 18:24:52 +0000

    hasta el ejercicio 6

---
+++ lol/EjecutarContInt.java
@@ -0,0 +1,44 @@
+
+/**
+ * EjecutarContInt.
+ * 
+ * @author (Jacobo Ravel L) 
+ * @version (2020/02/03)
+ */
+import java.util.*;
+
+public class EjecutarContInt
+{
+    public static void main(String args[]){
+    
+    Scanner scan = new Scanner(System.in);
+    int c,i;
+   
+    System.out.println("ingrese valor inicial del contador: ");
+    c = scan.nextInt();
+    System.out.println("ingrese el incremento del contador: ");
+    i = scan.nextInt();
+    
+    ContIncr contIn = new ContIncr(c,i);
+    contIn.incr();
+    contIn.incr();
+    System.out.printf("\nvalor del contador: %05d", contIn.obtCont());
+    contIn.reiniciar();
+    contIn.incr();
+    contIn.incr();
+    contIn.incr();
+    System.out.printf("\nvalor del contador: %03d",contIn.obtCont());
+    System.out.println("\nNuevo valor del incremento : ");
+    i=scan.nextInt();
+    contIn.estIncr(i);
+    contIn.incr();
+    contIn.incr();
+    contIn.incr();
+    System.out.printf("Valor contador: %04d ",contIn.obtCont());
+    
+    
+    
+    
+    
+    }
+}
