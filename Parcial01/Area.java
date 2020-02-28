4a684abb4a9d3f4799b5cc232d34c1791ba0f945
jravel <jravel@53118b7d-0bdd-47fb-8e37-54b3e301ef77>
2020-02-28 18:27:28 +0000

    Envio de parcial

--- Parcial01/Area.java
+++ Parcial01/Area.java
@@ -1,18 +1,26 @@
 /**
  * Write a description of class Area here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Jacobo Rave Londoño)
+ * @version (2020/02/28)
  */
 public class Area {
 
+    private double xInf;
+    private double yInf;
+    private double xSup;
+    private double ySup;
+    private int golpes;
+    
     public Area(float x1, float y1, float x2, float y2) {
+        
     }
 
     public void golpear(float x, float y) {
     }
 
     public int obtTotalGolpes() {
+        
     }
 
 }
--- Parcial01/CosteDestruccion.java
+++ Parcial01/CosteDestruccion.java
@@ -1,15 +1,36 @@
 /**
  * Write a description of class CosteDestruccion here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Jacobo Rave Londoño)
+ * @version (2020/02/28 )
  */
 import java.util.Scanner;
 
 public class CosteDestruccion {
 
     public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-
+        Scanner scan = new Scanner(System.in);
+        int km,arma; long acumulador=0;
+        long costo1=0,costo2=0,costo3=0,costototal=0;
+        
+        System.out.print("Ingrese el numero de kilometros cuadrados que quiere destruir  ");
+        km=scan.nextInt();
+        System.out.println("Ingrese el tipo de arma");
+        arma=scan.nextInt();
+        
+         while(scan.hasNextInt()){
+           
+         if(arma==0)
+          costo1=(km*300000);
+         else if(arma==1)
+          costo2=(km*100000);
+         else if(arma==2)
+          costo3=(km*100);
+          km=scan.nextInt();
+          arma=scan.nextInt();
+          acumulador=costo1+costo2+costo3;
+        }
+        costototal=costo1+costo2+costo3;
+        System.out.print("Costo= "+ costototal);
     }
 }
