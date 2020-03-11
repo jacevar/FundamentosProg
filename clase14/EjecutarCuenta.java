bc25f5f513df692aac8a8e9f58209aa830b3ea2e
jravel <jravel@53118b7d-0bdd-47fb-8e37-54b3e301ef77>
2020-03-11 18:29:30 +0000

    clase 14

---
+++ clase14/EjecutarCuenta.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class EjecutarCuenta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EjecutarCuenta
+{
+   public static void main(String args[]){
+       
+     Cuenta c1=new Cuenta();  
+     Cuenta c2=new Cuenta(); 
+     Cuenta c3=new Cuenta(); 
+       
+     Cuenta.imprimirNroCuenta(10, c1);  
+     Cuenta.imprimirNroCuenta(5, c2);  
+     Cuenta.imprimirNroCuenta(6, c3);  
+        
+    }
+}
