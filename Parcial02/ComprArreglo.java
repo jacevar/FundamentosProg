fcfacc9a6a585372a1483b871ccdac6f315366c6
jravel <jravel@53118b7d-0bdd-47fb-8e37-54b3e301ef77>
2020-04-18 03:03:00 +0000

    Envío parcial 2 con los 3 puntos

--- Parcial02/ComprArreglo.java
+++ Parcial02/ComprArreglo.java
@@ -1,46 +1,56 @@
+public class ComprArreglo {
+    static int i =0;
+    static int init=1;
+    static int PosIni=0,PosFin=0;
+    static int j = 0;
 
-public class ComprArreglo
-{
-    static double a=0;
-    static int cont=0;
-    static int b=0;
-    static boolean encontrado=false;
-    static int i=0;
-    public static int comprArreglo(double arreglo[]) {
-        for(int j =0;j<arreglo.length-1;j++){
-            if(arreglo[j]==arreglo[j+1]){
-                
-                cont++;
-                a=arreglo[j];
-            }  
-
-        }
-        double arr[]=new double[cont];
-
-        for(int k=0;k<arr.length;k++){
-            arr[k]=a;
-
-        }
-
-        imprArreglo(arr, a);
+    public static int compararArreglo(double arreglo[]) {
         
-     }
-    public static void imprArreglo(double arr[], double a) {
+        for( i=0;i<arreglo.length-1 && PosIni<arreglo.length;i++){
+            if(arreglo[i]==arreglo[PosIni+1]){
+
+                for( j=PosIni+1; j<arreglo.length && arreglo[i]==arreglo[j]; j++){
+                    PosFin=j; 
+
+                }
+                PosIni=PosFin+1;
+                if(PosIni<arreglo.length){
+
+                    arreglo[init]=arreglo[PosIni];
+                    init++;
+
+                }
+            }
+            else{
+                PosIni++;
+                arreglo[init]=arreglo[PosIni];
+                init++;
+
+            }   
+            
+        }
+
+        return init;
+    }
+
+    public static void imprArreglo(double arreglo[], int n) {
         System.out.print("[");
-        for (int i = 0; i < a; i++) {
-            System.out.print(arr[i]);
-            if ((i + 1) < a)
+        for (int i = 0; i < n; i++) {
+            System.out.print(arreglo[i]); 
+            if ((i + 1) < n)
                 System.out.print(",");
         }
         System.out.println("]");
     }
 
-    
-       public static void main(String[] args) {
-	double arreglo[] = { 1.23, 1.23, 12.34, 12.34, 12.34, 23.23, 1.23, 1.23 };
-	imprArreglo(arreglo, arreglo.length);
-	int tam = comprArreglo(arreglo);
-	imprArreglo(arreglo, tam);
+    public static void main(String[] args) {
+        double arreglo[] = { 1.23, 1.23, 12.34, 12.34, 12.34, 23.23, 1.23, 1.23 };
+        imprArreglo(arreglo, arreglo.length);
+        int tam = compararArreglo(arreglo);
+        imprArreglo(arreglo, tam);
     }
 }
-}
+
+   
+     
+     
--- Parcial02/Ventas.java
+++ Parcial02/Ventas.java
@@ -1,16 +1,29 @@
+
+
 public class Ventas {
     int codigo;
-    double ventas[];
-
+    double ventas[]=new double[4];
+    
     public Ventas(int codigo, double tr1, double tr2, double tr3, double tr4) {
+        //recibe codigo y venta de 4 trimestres
+        this.codigo=codigo;
+        //double ventas[]={tr1,tr2,tr3,tr4}; no me sirvio esta declaracion :(
+        ventas[0] =tr1;
+        ventas[1] =tr2;
+        ventas[2] =tr3;
+        ventas[3] =tr4;
     }
 
     public int obtCodigo() {
+        return codigo;
     }
 
     public double obtVentas(int tr) {
+        return ventas[tr];
     }
 
-    public String toString() {
+     public String toString() {
+        return  codigo+""+" "+ventas[0]+" "+ventas[1]+" "+ventas[2]+" "+ventas[3];
+        
     }
 }
