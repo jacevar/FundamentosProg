9405334d7b1c5635be550e38371668bc251d04c1
jravel <jravel@53118b7d-0bdd-47fb-8e37-54b3e301ef77>
2020-04-24 17:27:28 +0000

    Intercambio inicial de proyecto

---
+++ clase 22/DeclaracionMatriz.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class DeclaracionMatriz here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DeclaracionMatriz
+{
+    public static void main
+}
