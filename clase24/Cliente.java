b12c846e342dadbbab4b50736dee76cedd9837b8
jravel <jravel@53118b7d-0bdd-47fb-8e37-54b3e301ef77>
2020-05-06 18:43:24 +0000

    clase 24

---
+++ clase24/Cliente.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Cliente here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import contadores.Contador;
+
+public class Cliente
+{
+   public static void main(String []args) {
+       Contador contador = new Contador(1, 2);
+       
+   }
+}
